ed45ccb6e5ecacd19299fe776578abeca2ca2e7f
Stanislav Sergienya <skilful522@gmail.com>
2019-07-16 18:21:43 +0300

    third commit

--- index.js
+++ index.js
@@ -47,3 +47,31 @@ posts.addEventListener("click", function () {
         searchImages[i].src = postImages[i];
     }
 });
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const port = process.env.PORT || 3000;
+
+const readFile = (url, response) => {
+    fs.readFile(url, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            response.statusCode = 204;
+            response.end(JSON.stringify(err));
+        } else {
+            console.log(url);
+            response.writeHead(200);
+            response.end(data);
+        }
+    });
+};
+
+const server = http.createServer(function (request, response) {
+    const path = url.parse(request.url, true).pathname.replace(/^\/+|\/+$/g, '');
+    readFile(path, response);
+});
+
+server.listen(port);
+console.log(`server started on ${port} port`);
